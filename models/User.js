04b15784f8d46b3a996c7b64dadd1205c3f7a999
Chris Gilbert <chrisgilbert4@icloud.com>
2018-12-09 14:19:35 +0000

    syntax fix

--- models/User.js
+++ models/User.js
@@ -6,7 +6,7 @@ var userSchema = new Schema({
     name: {type: String, required: true},
     username: {type: String, required: true},
     email: {type: String, required: true},
-    password: {type: String, required: true},
+    password: {type: String, required: true}
   }
 });
 
