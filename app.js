e64488e1c9c5b7ce5cc3cb1223664f0218e604a8
James de Winton <james.dewinton@gmail.com>
2018-12-09 12:01:05 +0000

    database connection

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var port = 3000
 var app = express();
+var mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var loginRouter = require('./routes/login');
@@ -9,5 +10,9 @@ var signupRouter = require('./routes/signup');
 // app.use('/', indexRouter);
 // app.use('/login', loginRouter);
 // app.use('/signup', signupRouter);
+
+mongoose.connect("mongodb://james:mousehouse12@ds249873.mlab.com:49873/kirills_bnb")
+  .then(() => console.log('connected..........'))
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
