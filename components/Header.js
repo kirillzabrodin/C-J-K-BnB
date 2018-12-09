42e6a0c7ef29109afba9a0caf1fdd22a70566c4d
Kirill Zabrodin <kirillvzabrodin@gmail.com>
2018-12-09 11:45:54 +0000

    views and components

--- components/Header.js
+++ components/Header.js
@@ -6,15 +6,18 @@ const linkStyle = {
 
 const Header = () => (
     <div>
-        <Link href="/">
-          <a style={linkStyle}>Home</a>
-        </Link>
-        <Link href="/log-in">
-          <a style={linkStyle}>Log-in</a>
-        </Link>
-        <Link href="/sign-up">
-          <a style={linkStyle}>Sign-up</a>
-        </Link>
+      <Link href="/">
+        <a style={linkStyle}>Home</a>
+      </Link>
+      <Link href="/log-in">
+        <a style={linkStyle}>Log-in</a>
+      </Link>
+      <Link href="/sign-up">
+        <a style={linkStyle}>Sign-up</a>
+      </Link>
+      <Link href="/new-space">
+        <a style={linkStyle}>New Space</a>
+      </Link>
     </div>
 )
 
--- pages/index.js
+++ pages/index.js
@@ -7,8 +7,28 @@ import { Button } from 'reactstrap'
 
 const Index = () => (
     <Layout>
-    <h1>Welcome to MakersBnB</h1>
-    <SignupForm />
+      <h1>Welcome to MakersBnB</h1>
+      <CardColumns>
+        <Card>
+          <CardBody>
+            <CardTitle></CardTitle>
+            <CardSubtitle></CardSubtitle>
+            <CardText></CardText>
+            <CardText></CardText>
+            <Button></Button>
+          </CardBody>
+        </Card>
+        <Card body inverse style={{ backgroundColor: '#333', borderColor: '#333' }}>
+          <CardTitle>Want to rent your own space?</CardTitle>
+          <CardText>Sign up now to get started.</CardText>
+          <Button>Sign up</Button>
+        </Card>
+        <Card body inverse color="primary">
+          <CardTitle>Become a host</CardTitle>
+          <CardText>Rent your space and disrupt your local rental market.</CardText>
+          <Button color="secondary">Oh hell yeah...</Button>
+        </Card>
+      </CardColumns>
     </Layout>
 )
 
