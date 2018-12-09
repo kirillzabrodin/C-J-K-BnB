8f3909875f1e2ae19677be307bbbdd76d0c2fdc5
Chris Gilbert <chrisgilbert4@icloud.com>
2018-12-09 12:44:04 +0000

    session added

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var app = express();
+var session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+}))
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
