c12b41b39951c55c79a1611b2cb70b5465e01aef
Chris Gilbert <chrisgilbert4@icloud.com>
2018-12-09 14:37:02 +0000

    mongoose added to app js

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 var express = require('express');
-var app = express();
 var session = require('express-session');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
+var app = express();
+
+app.use(bodyParser.json());
 
 app.use(session({
   secret: 'keyboard cat',
   resave: false,
   saveUninitialized: true
 }))
+
+mongoose.connect("mongodb://james:mousehouse12@ds249873.mlab.com:49873/kirills_bnb", { useNewUrlParser: true })
+ .then(() => console.log('connected to db...'));
+ .catch(err => console.log(err));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
