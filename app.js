4152b99a77574cd85d7168c5a2f5270cf0aa3753
Kirill Zabrodin <kirillvzabrodin@gmail.com>
2018-12-09 17:01:57 +0000

    added app stuff

--- app.js
+++ app.js
@@ -1,22 +1,4 @@
 var express = require('express');
-<<<<<<< HEAD
-var bodyParser = require('body-parser');
-var path = require('path');
-var session = require('express-session')
-var app = express();
-
-app.use(bodyParser.json());
-
-app.use(express.static(path.join(__dirname, 'src')));
-
-app.use(session({
-  secret: "test"
-}))
-
-var router = require('./router.js');
-app.use('/', router);
-
-=======
 var session = require('express-session');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
@@ -24,6 +6,7 @@ var bodyParser = require('body-parser');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var spacesRouter = require('./routes/spaces');
+var router = require('./router.js');
 
 var app = express();
 
@@ -35,15 +18,16 @@ app.use(session({
   saveUninitialized: true
 }))
 
+app.use(express.static(path.join(__dirname, 'src')));
+
 mongoose.connect("mongodb://james:mousehouse12@ds249873.mlab.com:49873/kirills_bnb", { useNewUrlParser: true })
     .then(() => console.log('MongoDB connected...'))
     .catch(err => console.log(err));
 
-app.use('/', indexRouter);
+app.use('/', router);
 app.use('/users', usersRouter);
 app.use('/spaces', spacesRouter);
 
 app.listen(5000, () => console.log(`C-J-K-BnB listening on port 5000!`))
 
->>>>>>> 6fda567f7b4b38921b7ee97167b72be3318d8e48
 module.exports = app;
